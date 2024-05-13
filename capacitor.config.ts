51ec0333cc4ce10494a12772c5f048882f0a8198
Andres Navarro S <andresm.navarro@gmail.com>
2024-05-13 16:39:49 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tcg-life-counter',
+  webDir: 'dist'
+};
+
+export default config;
